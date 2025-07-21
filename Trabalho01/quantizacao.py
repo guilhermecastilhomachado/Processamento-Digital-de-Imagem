2bbe9ae7e3b6968f4672314e86e5cb1ac693a045
Guilherme Castilho Machado <guiguicastilho34@gmail.com>
2025-07-21 19:51:53 -0300

    fazendo alguns ajustes na tarefa 2 do trabalho 1

--- Trabalho01/quantizacao.py
+++ Trabalho01/quantizacao.py
@@ -2,6 +2,11 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Exercicio 2 da tarefa 1 - Quantização de Imagem
+
+# Utilize a imagem ctskull-256.tif e refaça a quantização de seus pixels
+# utilizando de 7 a 1 bit(s) por pixel.
+
 # Carrega a imagem em escala de cinza
 imagem = cv2.imread('ctskull-256.tif', cv2.IMREAD_GRAYSCALE)
 
@@ -14,19 +19,19 @@ def quantizar_imagem(imagem, bits):
     imagem_quantizada = np.floor(imagem / (256 / niveis)) * (255 / (niveis - 1))
 
     # Converte para uint8
-    return imagem_quantizada.astype(np.uint8)
-
+    return imagem_quantizada.astype(np.uint8) # Converte a imagem quantizada para o tipo uint8
+    # essa conversão é necessária para garantir que os valores da imagem estejam no intervalo correto de 0 a 255
 
 # Exibir todas as quantizações de 7 a 1 bits
 plt.figure(figsize=(12, 10))
-for i, bits in enumerate(range(7, 0, -1), 1):
-    img_q = quantizar_imagem(imagem, bits)
+for i, bits in enumerate(range(7, 0, -1), 1): # Cria um loop para quantizar a imagem de 7 a 1 bits
+    img_q = quantizar_imagem(imagem, bits) # Chama a função para quantizar a imagem com o número de bits especificado
 
     # Mostrar na tela
-    plt.subplot(3, 3, i)
-    plt.imshow(img_q, cmap='gray')
-    plt.title(f'{bits} bit(s)')
-    plt.axis('off')
+    plt.subplot(3, 3, i) # o subplot dividir a tela em várias partes, neste caso 3 linhas e 3 colunas
+    plt.imshow(img_q, cmap='gray') # imshow exibe a imagem, cmap='gray' indica que é uma imagem em escala de cinza
+    plt.title(f'{bits} bit(s)') # título da imagem exibida
+    plt.axis('off') # desativa os eixos para uma melhor visualização
 
-plt.tight_layout()
-plt.show()
+plt.tight_layout() # ajusta o layout para evitar sobreposição de títulos
+plt.show() # Exibe a imagem original
